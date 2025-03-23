3a692eb548980504d12c33fa818ac01717ba5d72
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2025-03-23 15:49:44 +0700

    clear starter kit dashboard

---
+++ resources/views/layouts/backend-layout.blade.php
@@ -0,0 +1,144 @@
+
+<!doctype html>
+<html lang="en" class="remember-theme">
+  <head>
+    <meta charset="utf-8">
+    <!--
+      Available classes for <html> element:
+
+      'dark'                  Enable dark mode - Default dark mode preference can be set in app.js file (always saved and retrieved in localStorage afterwards):
+                                window.Codebase = new App({ darkMode: "system" }); // "on" or "off" or "system"
+      'dark-custom-defined'   Dark mode is always set based on the preference in app.js file (no localStorage is used)
+      'remember-theme'        Remembers active color theme between pages using localStorage when set through
+                                - Theme helper buttons [data-toggle="theme"]
+    -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <title>UPUCC Dashboard</title>
+
+    <meta name="description" content="Codebase - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="index, follow">
+
+    <!-- Open Graph Meta -->
+    <meta property="og:title" content="Codebase - Bootstrap 5 Admin Template &amp; UI Framework">
+    <meta property="og:site_name" content="Codebase">
+    <meta property="og:description" content="Codebase - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="{{asset('assets/media/favicons/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{asset('assets/media/favicons/favicon-192x192.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/media/favicons/apple-touch-icon-180x180.png')}}">
+    <!-- END Icons -->
+
+    <!-- Stylesheets -->
+
+    <!-- Codebase framework -->
+    <link rel="stylesheet" id="css-main" href="{{asset('assets/css/codebase.min.css')}}">
+
+    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
+    <!-- END Stylesheets -->
+
+    <!-- Load and set color theme + dark mode preference (blocking script to prevent flashing) -->
+    <script src="{{asset('assets/js/setTheme.js')}}"></script>
+  </head>
+
+  <body>
+    <!-- Page Container -->
+    <!--
+      Available classes for #page-container:
+
+      SIDEBAR & SIDE OVERLAY
+
+        'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+        'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+        'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+        'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+        'sidebar-dark'                              Dark themed sidebar
+
+        'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+        'side-overlay-o'                            Visible Side Overlay by default
+
+        'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+        'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+      HEADER
+
+        ''                                          Static Header if no class is added
+        'page-header-fixed'                         Fixed Header
+
+      HEADER STYLE
+
+        ''                                          Classic Header style if no class is added
+        'page-header-modern'                        Modern Header style
+        'page-header-dark'                          Dark themed Header (works only with classic Header style)
+        'page-header-glass'                         Light themed Header with transparency by default
+                                                    (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+        'page-header-glass page-header-dark'        Dark themed Header with transparency by default
+                                                    (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+      MAIN CONTENT LAYOUT
+
+        ''                                          Full width Main Content if no class is added
+        'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+        'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+    -->
+    <div id="page-container" class="sidebar-o enable-page-overlay side-scroll page-header-modern main-content-boxed">
+      <!-- Side Overlay-->
+     @include('layouts.backend.aside')
+      <!-- END Side Overlay -->
+
+      <!-- Sidebar -->
+      <!--
+        Helper classes
+
+        Adding .smini-hide to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
+        Adding .smini-show to an element will make it visible (opacity: 1) when the sidebar is in mini mode
+          If you would like to disable the transition, just add the .no-transition along with one of the previous 2 classes
+
+        Adding .smini-hidden to an element will hide it when the sidebar is in mini mode
+        Adding .smini-visible to an element will show it only when the sidebar is in mini mode
+        Adding 'smini-visible-block' to an element will show it (display: block) only when the sidebar is in mini mode
+      -->
+     @include('layouts.backend.sidebar')
+      <!-- END Sidebar -->
+
+      <!-- Header -->
+      @include('layouts.backend.header')
+      <!-- END Header -->
+
+      <!-- Main Container -->
+      <main id="main-container">
+        <!-- Page Content -->
+        @yield('content')
+        <!-- END Page Content -->
+      </main>
+      <!-- END Main Container -->
+
+      <!-- Footer -->
+    @include('layouts.backend.footer')
+      <!-- END Footer -->
+    </div>
+    <!-- END Page Container -->
+
+    <!--
+        Codebase JS
+
+        Core libraries and functionality
+        webpack is putting everything together at assets/_js/main/app.js
+    -->
+    <script src="{{asset('assets/js/codebase.app.min.js')}}"></script>
+
+    <!-- Page JS Plugins -->
+    <script src="{{asset('assets/js/plugins/chart.js/chart.umd.js')}}"></script>
+
+    <!-- Page JS Code -->
+    <script src="{{asset('assets/js/pages/be_pages_dashboard.min.js')}}"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('backend.dashboard');
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
